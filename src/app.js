71aaad618ea66a3a4fafd5b60d743755f2fe89f7
Olessia Potapova <olessia.potapova@gmail.com>
2018-08-31 12:00:49 -0300

    displays Pet API name, animal and breed in browser

--- src/app.js
+++ src/app.js
@@ -10,17 +10,58 @@ const petfinder = pf({
 });
 
 class App extends React.Component {
-  //rendered to the dom for the first time, now ready to do other stuff
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
   componentDidMount() {
-    petfinder.breed.list({ animal: "dog" }).then(console.log, console.error);
+    petfinder.pet
+      .find({ location: "Seattle, WA", output: "full" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          //// if multiple array of multiple pets exists
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            //turns into array if 1 breed returned
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          //if finds nothing
+          pets = [];
+        }
+        this.setState({
+          // give it object of things to update, a shallow merge
+          pets
+        });
+      });
   }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havansese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Pumpkin" animal="cat" breed="Orange Tab" />
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id} //must be unique
+                name={pet.name}
+                animal={pet.animal}
+                breed={breed}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
