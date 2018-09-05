58f1553f9f8cc373c14f2553bcbfbace25051fbf
Olessia Potapova <olessia.potapova@gmail.com>
2018-09-05 14:50:44 -0300

    moved all markup from SearchParams to SearchBox

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,54 +1,11 @@
 import React from "react";
+import SearchBox from "./SearchBox";
 
 class SearchParams extends React.Component {
   render() {
     return (
-      <div className="search-params">
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            value={this.state.location}
-            placeholder="Location"
-            onChange={this.handleLocationChange}
-          />
-        </label>
-
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={this.state.animal}
-            onChange={this.handleAnimalChange}
-            onBlur={this.handleAnimalChange}
-          >
-            <option />
-            {ANIMALS.map(animal => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-
-        <label htmlFor="breed">
-          Breed
-          <select
-            disabled={!this.state.breeds.length}
-            id="breed"
-            value={this.state.breeds.breed}
-            onChange={this.handleBreedChange}
-            onBlur={this.handleBreedChange}
-          >
-            <option />
-            {this.state.breeds.map(breed => (
-              <option key={breed} value={breed}>
-                {breed}
-              </option>
-            ))}
-          </select>
-        </label>
-        <button>Submit</button>
+      <div className="search-route">
+        <SearchBox />
       </div>
     );
   }
